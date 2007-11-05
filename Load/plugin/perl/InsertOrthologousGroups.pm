cca7c2de16795d47c7a74ffed2c1b8cf0966b6e8
Xin Gao <xingao@upenn.edu>
2007-11-05 17:21:18 +0000

    add scripts and plugins for OrthoMCL resources and analysis steps
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/OrthoMCLData/trunk@19001 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ Load/plugin/perl/InsertOrthologousGroups.pm
@@ -0,0 +1,190 @@
+package OrthoMCLData::Load::Plugin::InsertOrthologousGroups;
+
+@ISA = qw(GUS::PluginMgr::Plugin);
+
+# ----------------------------------------------------------------------
+
+use strict;
+use GUS::PluginMgr::Plugin;
+use FileHandle;
+
+use GUS::Model::ApiDB::OrthologGroup;
+use GUS::Model::ApiDB::OrthologGroupAaSequence;
+
+use ApiCommonData::Load::Util;
+
+
+my $argsDeclaration =
+[
+    fileArg({name           => 'orthoFile',
+            descr          => 'Ortholog Data (ortho.mcl). OrthologGroupName(gene and taxon count) followed by a colon then the ids for the members of the group',
+            reqd           => 1,
+            mustExist      => 1,
+	    format         => 'ORTHOMCL9(446 genes,1 taxa): osa1088(osa) osa1089(osa) osa11015(osa)...',
+            constraintFunc => undef,
+            isList         => 0, }),
+
+
+ stringArg({ descr => 'Name of the External Database',
+	     name  => 'extDbName',
+	     isList    => 0,
+	     reqd  => 1,
+	     constraintFunc => undef,
+	   }),
+
+
+ stringArg({ descr => 'Version of the External Database Release',
+	     name  => 'extDbVersion',
+	     isList    => 0,
+	     reqd  => 1,
+	     constraintFunc => undef,
+	   })
+];
+
+my $purpose = <<PURPOSE;
+The purpose of this plugin is to insert complete rows representing orthologous groups.  Each time the plugin is run, a new ApiDB::OrthologGroup is inserted.  Each child of OrthologGroup represents a line from the orthologFile (ie an orthologGroup).  For each orthoId which can be mapped to a source_id a row in ApiDB::OrthologGroupAASequence is inserted.
+PURPOSE
+
+my $purposeBrief = <<PURPOSE_BRIEF;
+Load an orthoMCL analysis result.
+PURPOSE_BRIEF
+
+my $notes = <<NOTES;
+Need a script to create the mapping file.
+NOTES
+
+my $tablesAffected = <<TABLES_AFFECTED;
+ApiDB.OrthologGroup,
+ApiDB.OrthologGroupAASequence
+TABLES_AFFECTED
+
+my $tablesDependedOn = <<TABLES_DEPENDED_ON;
+Sres.TaxonName,
+Sres.ExternalDatabase,
+Sres.ExternalDatabaseRelease,
+Sres.ExternalAASequence
+TABLES_DEPENDED_ON
+
+my $howToRestart = <<RESTART;
+The plugin can been restarted, since the same ortholog group from the same OrthoMCL analysis version will only be loaded once.
+RESTART
+
+my $failureCases = <<FAIL_CASES;
+
+FAIL_CASES
+
+my $documentation = { purpose          => $purpose,
+                      purposeBrief     => $purposeBrief,
+                      notes            => $notes,
+                      tablesAffected   => $tablesAffected,
+                      tablesDependedOn => $tablesDependedOn,
+                      howToRestart     => $howToRestart,
+                      failureCases     => $failureCases };
+
+# ----------------------------------------------------------------------
+
+sub new {
+  my ($class) = @_;
+  my $self = {};
+  bless($self,$class);
+
+  $self->initialize({ requiredDbVersion => 3.5,
+                      cvsRevision       => '$Revision: 16406 $',
+                      name              => ref($self),
+                      argsDeclaration   => $argsDeclaration,
+                      documentation     => $documentation});
+
+  return $self;
+}
+
+# ======================================================================
+
+sub run {
+    my ($self) = @_;
+
+    my $orthologFile = $self->getArgs()->{orthoFile};
+    my $dbReleaseId = $self->getExtDbRlsId($self->getArgs()->{extDbName}, 
+ 				           $self->getArgs()->{extDbVersion});
+
+    $self->log("Loading ortholog group file");
+
+    # parse group file
+    my $groupCount = $self->_parseGroupFile($orthologFile, $dbReleaseId);
+
+    $self->log("Total $groupCount ortholog groups loaded.");
+}
+
+
+# ----------------------------------------------------------------------
+
+sub _parseGroupFile {
+    my ($self, $orthologFile, $dbReleaseId) = @_;
+
+    open ORTHO_FILE, "<$orthologFile";
+    my $groupCount = 0;
+    while (<ORTHO_FILE>) {
+        chomp;
+        next if ($_ == '');
+
+        if ($self->_parseGroup($_, $dbReleaseId)) {
+            $groupCount++;
+            
+            if ($groupCount % 1000 == 0) {
+                $self->log("$groupCount ortholog groups loaded.");
+            }
+        }
+    }
+    return $groupCount;
+}
+
+
+sub _parseGroup {
+    my ($self, $line, $dbReleaseId) = @_;
+    
+    # example line: ORTHOMCL79685(2 genes,2 taxa): 81703(spn) 32290(ban)
+    if (/^(\S+)\((\d+) genes\,(\d) taxa\)\:(.*)/) {
+        my $groupName = $1;
+        my $geneCount = $2;
+        my $taxonCount = $3;
+        my @genes = split(' ', $4);
+
+        print "group=$groupName, #genes=$geneCount, #taxon=$taxonCount\n";
+
+        # create a OrthlogGroup instance
+        my $orthoGroup = GUS::Model::ApiDB::OrthologGroup->
+            new({name => $groupName,
+                 number_of_members => $geneCount,
+                 external_database_release_id => $dbReleaseId,
+                });
+
+        for (@genes) {
+            if (/~(\S+)\((\S+)\)/) {
+		my $sequenceId = $1;
+		my $taxonId = $2;
+            
+		print "gene=$sequenceId, taxon=$taxonId; ";
+
+		# create a OrthologGroupAASequence instance
+		my $orthoGroupSequence = GUS::Model::ApiDB::OrthologGroupAASequence->
+		    new ({aa_sequence_id => $sequenceId,
+		      })->setParent($orthoGroup);
+	    }
+        }
+        print "\n";
+        # $orthoGroup->submit();
+        $orthoGroup->undefPointerCache();
+    } 
+}
+
+
+# ----------------------------------------------------------------------
+
+sub undoTables {
+  my ($self) = @_;
+
+  return ('ApiDB.OrthologGroupAASequence',
+          'ApiDB.OrthologGroup',
+	 );
+}
+
+1;
