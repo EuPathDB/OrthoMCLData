ff0acc5bad0192e61b2823b3e75e6a8b82507912
Charles Treatman <ctreatma@_.com>
2008-01-23 20:23:27 +0000

    setting properties
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/OrthoMCLData/trunk@19911 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Load/plugin/perl/InsertOrthomclResource.pm
+++ Load/plugin/perl/InsertOrthomclResource.pm
@@ -76,11 +76,11 @@ sub new {
   bless($self,$class);
 
   $self->initialize({ requiredDbVersion => 3.5,
-                      cvsRevision       => '$Revision: 19800 $',
+                      cvsRevision       => '$Revision$',
                       name              => ref($self),
                       argsDeclaration   => $argsDeclaration,
                       documentation     => $documentation});
-
+ 
   return $self;
 }
 
