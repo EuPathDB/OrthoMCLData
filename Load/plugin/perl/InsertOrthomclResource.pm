31e6eea1c47626fd9de588b2d899c6a125678fb4
Charles Treatman <ctreatma@_.com>
2008-01-23 16:50:26 +0000

    Perl code for loading resources into resource table
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/OrthoMCLData/trunk@19901 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ Load/plugin/perl/InsertOrthomclResource.pm
@@ -0,0 +1,149 @@
+package OrthoMCLData::Load::Plugin::InsertOrthomclTaxon;
+
+@ISA = qw(GUS::PluginMgr::Plugin);
+
+# ----------------------------------------------------------------------
+
+use strict;
+use GUS::PluginMgr::Plugin;
+use FileHandle;
+
+use GUS::Model::ApiDB::OrthomclResource;
+
+use ApiCommonData::Load::Util;
+use Data::Dumper;
+
+my $argsDeclaration =
+[
+    fileArg({name           => 'resourceFile',
+            descr          => 'a tab-delimited file containing the resources',
+            reqd           => 1,
+            mustExist      => 1,
+	    format         => 'see Notes',
+            constraintFunc => undef,
+            isList         => 0, }),
+];
+
+
+my $purpose = <<PURPOSE;
+Insert the Orthomcl-DB specific resource information.  The resource input file is tab-delimited, with each line representing a single resource, providing the name and url of the data source for each specie.
+PURPOSE
+
+my $purposeBrief = <<PURPOSE_BRIEF;
+Insert the Orthomcl-DB specific resource information used by the OrthoMCL Data Source page.  
+PURPOSE_BRIEF
+
+my $notes = <<NOTES;
+The speciesFile is a columnar file with these columns:
+  - three_letter_abbrev
+  - source_name
+  - source_url
+
+
+
+NOTES
+
+my $tablesAffected = <<TABLES_AFFECTED;
+ApiDB.OrthomclResource,
+TABLES_AFFECTED
+
+my $tablesDependedOn = <<TABLES_DEPENDED_ON;
+ApiDB.OrthomclTaxon,
+
+TABLES_DEPENDED_ON
+
+my $howToRestart = <<RESTART;
+Use the Undo plugin first.
+RESTART
+
+my $failureCases = <<FAIL_CASES;
+
+FAIL_CASES
+
+my $documentation = { purpose          => $purpose,
+                      purposeBrief     => $purposeBrief,
+                      notes            => $notes,
+                      tablesAffected   => $tablesAffected,
+                      tablesDependedOn => $tablesDependedOn,
+                      howToRestart     => $howToRestart,
+                      failureCases     => $failureCases };
+
+# ----------------------------------------------------------------------
+
+sub new {
+  my ($class) = @_;
+  my $self = {};
+  bless($self,$class);
+
+  $self->initialize({ requiredDbVersion => 3.5,
+                      cvsRevision       => '$Revision: 19800 $',
+                      name              => ref($self),
+                      argsDeclaration   => $argsDeclaration,
+                      documentation     => $documentation});
+
+  return $self;
+}
+
+# ======================================================================
+
+sub run {
+    my ($self) = @_;
+
+    my $resourceFile = $self->getArgs()->{resourceFile};
+
+    open(FILE, $resourceFile) || $self->userError("can't open resource file '$resourceFile'");
+
+    while(<FILE>) {
+	chomp;
+	my $resource = parseResourceLine($_);
+	$resource->submit();
+    }
+}
+
+sub parseResourceLine {
+    my ($self, $line) = @_;
+
+    my $resource = GUS::Model::ApiDB::OrthomclResource->new();
+    my @resData = split('\t',$line);
+
+    my $dbh = $self->getQueryHandle();
+    my $sql = "SELECT orthomcl_taxon_id
+               FROM ApiDB.OrthomclTaxon
+               WHERE three_letter_abbrev = ?";
+   
+    my ($stmt) = $dbh->prepare($sql);
+
+    if (length($resData) == 3) {
+	my $taxonId = $self->getTaxonId($stmt, $resData[0]);
+	$resource->setOrthomclTaxonId($taxonId);
+	$resource->setResourceName($resData[1]);
+	$resource->setResourceUrl($resData[2]);
+    }
+    else {
+	$self->userError("invalid line in resource file: '$line'");
+    }
+    
+    return $resource;
+}
+
+sub getTaxonId {
+    my ($self, $stmt, $abbrev) = @_;
+
+    my @id = $self->sqlAsArray( Handle => $stmt, Bind => [$abbrev] );
+
+    if (scalar @id != 1) {
+	$self->error("Should return one value for three_letter_abbrev '$abbrev'");
+    }
+    return @id;
+}
+
+sub undoTables {
+  my ($self) = @_;
+
+  return ('ApiDB.OrthomclResource',
+	 );
+}
+
+
+
+1;
