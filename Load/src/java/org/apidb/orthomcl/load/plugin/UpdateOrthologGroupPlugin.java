0e43ec56770a7f3872795acda7fb1da234bd6be0
Charles Treatman <ctreatma@_.com>
2008-02-26 15:42:14 +0000

    fixed PValue constructor
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/OrthoMCLData/trunk@20546 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Load/src/java/org/apidb/orthomcl/load/plugin/UpdateOrthologGroupPlugin.java
+++ Load/src/java/org/apidb/orthomcl/load/plugin/UpdateOrthologGroupPlugin.java
@@ -29,7 +29,7 @@ public class UpdateOrthologGroupPlugin implements Plugin {
         int Exponent;
 
         PValue() {
-            Exponent = 0;
+            Exponent = 1;
             Mantissa = 0;
         }
 
