5ad3a1aa21ed6501dc76af3e6b7b061afabed14e
Steve Fischer <sfischer@upenn.edu>
2008-08-27 15:09:39 +0000

    debug, using new input file format
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/OrthoMCLData/trunk@23072 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Load/plugin/perl/InsertOrthologousGroupsFromMcl.pm
+++ Load/plugin/perl/InsertOrthologousGroupsFromMcl.pm
@@ -1,4 +1,4 @@
-package OrthoMCLData::Load::Plugin::InsertOrthologousGroups;
+package OrthoMCLData::Load::Plugin::InsertOrthologousGroupsFromMcl;
 
 @ISA = qw(GUS::PluginMgr::Plugin);
 
@@ -40,17 +40,10 @@ my $argsDeclaration =
 	     constraintFunc => undef,
 	   }),
 
- stringArg({ descr => 'List of taxon abbrevs we want to load (eg: pfa, pvi)',
-	     name  => 'taxaToLoad',
-	     isList    => 1,
-	     reqd  => 1,
-	     constraintFunc => undef,
-	   }),
-
 ];
 
 my $purpose = <<PURPOSE;
-The purpose of this plugin is to insert complete rows representing orthologous groups.  Each time the plugin is run, a new ApiDB::OrthologGroup is inserted.  Each child of OrthologGroup represents a line from the orthologFile (ie an orthologGroup).  For each orthoId which can be mapped to a source_id a row in ApiDB::OrthologGroupAASequence is inserted.
+Insert an ApiDB::OrthologGroup and its members from an orthomcl groups file.
 PURPOSE
 
 my $purposeBrief = <<PURPOSE_BRIEF;
@@ -116,30 +109,16 @@ sub run {
     my $dbReleaseId = $self->getExtDbRlsId($self->getArg('extDbName'), 
  				           $self->getArg('extDbVersion'));
 
-    my $taxaToLoad = $self->getArg('taxaToLoad');
-
-    $self->log("Loading ortholog group file");
-
-    # parse group file
-    $self->_parseGroupFile($orthologFile, $dbReleaseId, $taxaToLoad);
-}
-
-
-# ----------------------------------------------------------------------
-
-sub _parseGroupFile {
-    my ($self, $orthologFile, $dbReleaseId, $taxaToLoad) = @_;
-
     open ORTHO_FILE, "<$orthologFile";
     my $groupCount = 0;
     my $lineCount = 0;
+    my $notFound;
     while (<ORTHO_FILE>) {
         chomp;
         $lineCount++;
 
-        if (1 == $self->_parseGroup($_, $dbReleaseId, $taxaToLoad)) {
+        if ($self->_parseGroup($_, $dbReleaseId)) {
             $groupCount++;
-            
             if (($groupCount % 1000) == 0) {
                 $self->log("$groupCount ortholog groups loaded.");
             }
@@ -152,12 +131,12 @@ sub _parseGroupFile {
 
 
 sub _parseGroup {
-    my ($self, $line, $dbReleaseId, $taxaToLoad) = @_;
-    
+    my ($self, $line, $dbReleaseId) = @_;
+
     # example line: OG2_1009: osa|ENS1222992 pfa|PF11_0844
     if ($line = /^(\S+)\: (.*)/) {
         my $groupName = $1;
-        my @genes = split(' ', $2);
+        my @genes = split(/\s+/, $2);
 	my $geneCount = scalar(@genes);
 
         # print "group=$groupName, #genes=$geneCount, #taxon=$taxonCount\n";
@@ -170,13 +149,11 @@ sub _parseGroup {
                 });
 
         for (@genes) {
-            if (/(\w+)\|(\w+)/) {
+            if (/(\w+)\|(\S+)/) {
 		my $taxonAbbrev = $1;
 		my $sourceId = $2;
-		next unless grep($taxonAbbrev, @$taxaToLoad);
-
-		my $sequenceId =
-		  ApiCommonData::Load::Util::getOneAASeqIdFromGeneId($self,$sourceId);
+		my $sequenceId = $self->getAASequenceId("$taxonAbbrev|$sourceId");
+		die "can't find an aa_sequence_id for $taxonAbbrev|$sourceId\n" if !$sequenceId;
 
 		# create a OrthologGroupAASequence instance
 		my $orthoGroupSequence = GUS::Model::ApiDB::OrthologGroupAaSequence->
@@ -193,6 +170,25 @@ sub _parseGroup {
     } else {
         return 0;
     }
+}
+
+# use full form of input id "pfa|PF11_0344"
+sub getAASequenceId {
+  my ($self, $inputId) = @_;
+
+  if (!$self->{idMap}) {
+    my $sql = "
+select aa_sequence_id, source_id, three_letter_abbrev
+from apidb.OrthomclTaxon ot, dots.ExternalAaSequence s
+where ot.taxon_id = s.taxon_id
+";
+
+    my $stmt = $self->prepareAndExecute($sql);
+    while (my ($sequenceId, $sourceId, $taxonId) = $stmt->fetchrow_array()) {
+      $self->{idMap}->{"$taxonId|$sourceId"} = $sequenceId;
+    }
+  }
+  return $self->{idMap}->{$inputId};
 }
 
 
