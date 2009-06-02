1e796f6841db8ccb0d2325b610c6550c506b5ce2
Steve Fischer <sfischer@upenn.edu>
2009-06-02 20:54:37 +0000

    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/OrthoMCLData/trunk@29299 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Load/lib/perl/ProteomeJobMgr.pm
+++ Load/lib/perl/ProteomeJobMgr.pm
@@ -11,11 +11,8 @@ sub getCluster {
 	my $clusterServer = $getConfig('clusterServer');
 	my $clusterUser = $getConfig('clusterUserName');
 	$self->{cluster} = GUS::Workflow::SshCluster->new($clusterServer,
-							      $clusterUser,
-							      $self);
-	} else {
-	    $self->{cluster} = GUS::Workflow::NfsCluster->new($self);
-	}
+							  $clusterUser,
+							  $self);
     }
     return $self->{cluster};
 }
@@ -27,5 +24,41 @@ sub runCmd {
     my $status = $? >> 8;
     $self->error("Failed with status $status running: \n$cmd") if ($status);
     return $output;
+}
+
+sub error {
+    my ($self, $msg) = @_;
+
+    confess "$msg\n\n";
+}
+
+sub runClusterTask {
+    my ($self, $user, $server, $processIdFile, $logFile, $controllerPropFile, $numNodes, $time, $queue, $ppn) = @_;
+
+    # if not already started, start it up (otherwise the local process was restarted)
+    if (!$self->clusterTaskRunning($processIdFile, $user, $server)) {
+	my $cmd = "workflowclustertask $propFile $logFile $processIdFile $numNodes $time $queue $ppn";
+	$self->runCmd($test, "ssh -2 $user\@$server '/bin/bash -login -c \"$cmd\"'&");
+    }
+
+
+    my $done = $self->runCmd($test, "ssh -2 $user\@$server '/bin/bash -login -c \"if [ -a $logFile ]; then tail -1 $logFile; fi\"'");
+    return $done && $done =~ /Done/;
+}
+
+sub clusterTaskRunning {
+    my ($self, $processIdFile, $user, $server) = @_;
+
+    my $processId = `ssh -2 $user\@$server 'if [ -a $processIdFile ];then cat $processIdFile; fi'`;
+
+    chomp $processId;
+
+    my $status = 0;
+    if ($processId) {
+      system("ssh -2 $user\@$server 'ps -p $processId > /dev/null'");
+      $status = $? >> 8;
+      $status = !$status;
+    }
+    return $status;
 }
 
