ee6d52e100206b847e98ba89ae6520f2005c3095
Deborah Pinney <pinney@upenn.edu>
2009-07-07 14:26:38 +0000

    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/OrthoMCLData/trunk@29914 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Load/plugin/perl/UpdateOrthologGroup.pm
+++ Load/plugin/perl/UpdateOrthologGroup.pm
@@ -16,44 +16,16 @@ use GUS::Model::SRes::ExternalDatabaseRelease;
 use ApiCommonData::Load::Util;
 
 
-my $argsDeclaration =
-[
-    fileArg({name           => 'orthoFile',
-            descr          => 'Ortholog Data (ortho.mcl). OrthologGroupName(gene and taxon count) followed by a colon then the ids for the members of the group',
-            reqd           => 1,
-            mustExist      => 1,
-	    format         => 'OG2_1009: osa|ENS1222992 pfa|PF11_0844...',
-            constraintFunc => undef,
-            isList         => 0, }),
-
-
- stringArg({ descr => 'Name of the External Database',
-	     name  => 'extDbName',
-	     isList    => 0,
-	     reqd  => 1,
-	     constraintFunc => undef,
-	   }),
-
-
- stringArg({ descr => 'Version of the External Database Release',
-	     name  => 'extDbVersion',
-	     isList    => 0,
-	     reqd  => 1,
-	     constraintFunc => undef,
-	   }),
-
-];
 
 my $purpose = <<PURPOSE;
-Insert an ApiDB::OrthologGroup and its members from an orthomcl groups file.
+update ApiDB::OrthologGroup and ApiDB::OrthologGroupAaSequence tablesxs.
 PURPOSE
 
 my $purposeBrief = <<PURPOSE_BRIEF;
-Load an orthoMCL analysis result.
+update apidb.orthologgroup average percent identity,average percent match, average mantissa, average exponent, average connectivity, number match pairs and apidb.orthologgroupaasequence.connectivity.
 PURPOSE_BRIEF
 
 my $notes = <<NOTES;
-Need a script to create the mapping file.
 NOTES
 
 my $tablesAffected = <<TABLES_AFFECTED;
@@ -62,14 +34,12 @@ ApiDB.OrthologGroupAASequence
 TABLES_AFFECTED
 
 my $tablesDependedOn = <<TABLES_DEPENDED_ON;
-Sres.TaxonName,
-Sres.ExternalDatabase,
-Sres.ExternalDatabaseRelease,
-Sres.ExternalAASequence
+ApiDB.OrthologGroup,
+ApiDB.OrthologGroupAASequence
 TABLES_DEPENDED_ON
 
 my $howToRestart = <<RESTART;
-The plugin can been restarted, since the same ortholog group from the same OrthoMCL analysis version will only be loaded once.
+The plugin can been restarted, update should only affect rows that have not been updated.
 RESTART
 
 my $failureCases = <<FAIL_CASES;
@@ -229,9 +199,9 @@ EOF
     }
   }
 
-  my $groupsUpdated += $self->updateOrthologGroup($groupId, $pairCount, $sumPercentIdentity, $sumPercentMatch, $sumEvalue, \%connectivity, $grpSize);
-
   my $grpAaSeqUpdated += $self->updateOrthologGroupAaSequences($seqIdArr, \%connectivity);
+
+  my $groupsUpdated += $self->updateOrthologGroup($groupId, $pairCount, $sumPercentIdentity, $sumPercentMatch, $sumEvalue, \%connectivity, $grpSize);
 
   return ($groupsUpdated,$grpAaSeqUpdated); 
 
