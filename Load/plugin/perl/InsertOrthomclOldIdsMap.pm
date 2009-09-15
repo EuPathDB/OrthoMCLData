57c10a424ca604820fd0757599a29158e58c301f
Deborah Pinney <pinney@upenn.edu>
2009-09-15 15:08:20 +0000

    no change
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/OrthoMCLData/trunk@31185 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Load/plugin/perl/InsertOrthomclOldIdsMap.pm
+++ Load/plugin/perl/InsertOrthomclOldIdsMap.pm
@@ -79,7 +79,7 @@ sub new {
   bless($self,$class);
 
   $self->initialize({ requiredDbVersion => 3.5,
-                      cvsRevision       => '$Revision: 31181 $',
+                      cvsRevision       => '$Revision:  $',
                       name              => ref($self),
                       argsDeclaration   => $argsDeclaration,
                       documentation     => $documentation});
